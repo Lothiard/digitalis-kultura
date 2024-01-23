6bf36c039f4a9d95ba39d061ea917206d0092b8e
Restye János Barnabás <97302839+Lothiard@users.noreply.github.com>
2024-01-23 09:55:43 +0100

    continued solving

--- 2020-okt/sorozatok.cpp
+++ 2020-okt/sorozatok.cpp
@@ -2,25 +2,56 @@
 #include <fstream>
 #include <sstream>
 #include <iomanip>
+#include <string>
 
 using namespace std;
 
+struct datum_t {
+    int ev, honap, nap;
+    bool elott(datum_t other) {
+        if (other.ev > this->ev) return false;
+        if (other.ev < this->ev) return true;
+        if (other.honap > this->honap) return false;
+        if (other.honap < this->honap) return true;
+        if (other.nap > this->nap) return false;
+        if (other.nap < this->nap) return true;
+    }
+
+    static datum_t konvert(string datum) {
+        int ev, honap, nap;
+
+        ev = atoi(datum.substr(0, 4).c_str());
+        honap = atoi(datum.substr(5, 2).c_str());
+        nap = atoi(datum.substr(8, 2).c_str());
+
+        return datum_t{ ev, honap, nap };
+    }
+};
+
 struct sorozatok {
-    string datum;
+    datum_t datum;
+    bool hasDatum;
     string cim;
     string evad;
     int hossz;
     bool nezett;
 }sorozat[400];
 
+
 int N;
 
 void f1() {
     ifstream f("lista.txt");
 
+    string barmi;
+
     int i = 0;
     while (!f.eof()) {
-        getline(f, sorozat[i].datum);
+        getline(f, barmi);
+        if (barmi != "NI") {
+            sorozat[i].datum = datum_t::konvert(barmi);
+            sorozat[i].hasDatum = true;
+        }
         getline(f, sorozat[i].cim);
         getline(f, sorozat[i].evad);
         f >> sorozat[i].hossz;
@@ -40,11 +71,11 @@ void f2() {
 
     int ismert = 0;
     for (int i = 0; i < N; i++) {
-        if (sorozat[i].datum != "NI") {
+        if (sorozat[i].hasDatum) {
             ismert++;
         }
     }
-    cout << "A lisï¿½ban " << ismert << " db vetï¿½tï¿½si dï¿½tummal rendelkezï¿½ epizï¿½d van.\n\n";
+    cout << "A lisában " << ismert << " db vetítési dátummal rendelkezö epizód van.\n\n";
 }
 
 void f3() {
@@ -57,7 +88,68 @@ void f3() {
         }
     }
 
-    cout << "A listï¿½ban lï¿½vï¿½ epizï¿½dok " << setprecision(4) << (float)megnezett / (float)N * 100 << "%-ï¿½t lï¿½tta.";
+    cout << "A listában lévö epizódok " << setprecision(4) << (float)megnezett / (float)N * 100 << "%-át látta.\n\n";
+}
+
+void f4() {
+    cout << "4. feladat\n";
+
+    int osszesperc = 0;
+
+    for (int i = 0; i < N; i++) {
+        if (sorozat[i].nezett) {
+            osszesperc += sorozat[i].hossz;
+        }
+    }
+
+    int nap, ora, perc;
+    nap = osszesperc / 60 / 24;
+    ora = osszesperc / 60 % 24;
+    perc = osszesperc % 60;
+    cout << "Sorozatnézéssel " << nap << " napot " << ora << " órát " << perc << " percet töltött.\n\n";
+}
+
+void f5() {
+    cout << "5. feladat\n";
+
+    string datum;
+    cout << "Adjon meg egy dátumot! ";
+    cin >> datum;
+
+    datum_t konvert = datum_t::konvert(datum);
+
+    for (int i = 0; i < N; i++) {
+        if (konvert.elott(sorozat[i].datum) && sorozat[i].nezett == 0 && sorozat[i].hasDatum) {
+            cout << sorozat[i].evad << " " << sorozat[i].cim << endl;
+        }
+    }
+
+    cout << endl;
+}
+
+string hetnapja(int ev, int honap, int nap) {
+    string napok[7] = { "v", "h", "k", "sze", "cs", "p", "szo" };
+    int honapok[12] = { 0, 3, 2, 5, 0, 3, 5, 1, 4, 6, 2, 4 };
+
+    if (honap < 3) {
+        ev = ev - 1;
+    }
+    return napok[(ev + ev / 4 - ev / 100 + ev / 400 + honapok[honap - 1] + nap) % 7];
+}
+
+void f7() {
+    cout << "7. feladat\n";
+    cout << "Adja meg a hét napját (pédául cs)! Nap= ";
+    string nap;
+    cin >> nap;
+    /*
+    for (int i = 0; i < N; i++) {
+        if (hetnapja() == nap) {
+
+        }
+    }
+    folyt
+    */
 }
 
 int main() {
@@ -66,4 +158,7 @@ int main() {
     f1();
     f2();
     f3();
+    f4();
+    f5();
+    f7();
 }
